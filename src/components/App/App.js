a914ed72af45c146bd4f7c77e0bf8df254376c16
Ethan Shin <shinhc5@gmail.com>
2020-09-28 22:02:49 +0900

    chore: set components structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,3 +29,46 @@ function App() {
 }
 
 export default App;
+
+/*
+
+<Provider store={store}>
+  <App>
+    <Route path="/event/:eventId">
+      <ModalContainer /> <-> [READ] getState() <-> RX
+      <UpdateButton onClick={e} /> -> [UPDATE] dispatch() -> RX -> update() -> FB -> [listener]
+      <DeleteButton onClick={e} /> -> [DELETE] dispatch() -> RX -> delete() -> FB -> [listener]
+    </Route>
+    <AppHeader>
+      <Link to="/event/new">
+        <NewEventButton />
+      </Link>
+      <ViewModeToggle /> **[localState]
+    </AppHeader>
+    <Switch>
+      <Route path="/calendar">
+        <Calendar>
+          <PreviousButton />
+          <EventContainer> <-> [READ] getState() <-> RX <-> on() <-> FB
+            <WeeklyCardList>
+              <DailyCard>
+                (<div>00:00</div> * 24)(?)
+              </DailyCard>
+            </WeeklyCardList>
+          </EventContainer>
+          <NextButton />
+        </Calendar>
+      </Route>
+      <Route path="/event/new">
+        <Link path="/calender">
+          <BackButton />
+        </Link>
+        <ModalContainer ?? /> -> [WRITE] dispatch() -> RX -> write() -> FB -> [listener]
+      </Route>
+      <Redirect to="/calendar">
+    </Switch>
+  </App>
+</Provider>
+
+
+*/
