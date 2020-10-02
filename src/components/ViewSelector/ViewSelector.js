ef1cd0268bef4382e81463cb0d11a59c781efbef
JIHYUNHA <humm223@gmail.com>
2020-10-02 18:52:32 +0900

    modified viewselector

--- src/components/ViewSelector/ViewSelector.js
+++ src/components/ViewSelector/ViewSelector.js
@@ -2,10 +2,16 @@ import React from "react";
 import styles from "./ViewSelector.module.css";
 
 export default function ViewSelector ({ changeView }) {
+  const handleChange = (event) => {
+    if (event.target.value === "weekly") {
+      changeView();
+    }
+  };
+
   return (
     <select
       name="calendarView"
-      onChange={changeView}
+      onChange={handleChange}
       className={styles.ViewSelector}
     >
       <option value="daily">Daily</option>
