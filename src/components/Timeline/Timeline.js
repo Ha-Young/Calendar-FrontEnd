fe378db0540eea829314ef4913f5235fe64275d6
woori9 <woori9@users.noreply.github.com>
2020-10-03 15:51:51 +0900

    daily page 이벤트 추가 기능

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -17,11 +17,60 @@ function Timeline({ showDailyPage, ...props }) {
   }, []);
 
   const updateDate = props.updateDateReducer;
+  const addedEvent = props.addEventReducer;
+  const year = new Date().getFullYear();
 
   function DailySchedulePage() {
-    const unitTime = time.map((time) => {
+    const todayEventTime = [];
+    const eventTimeIndexArray = [];
+    let newDateFormat;
+    let startTime;
+    let endTime;
+
+    for (let keys in addedEvent) {
+      let date = addedEvent[keys].date.split('-');
+      if (String(updateDate.date).length === 1) {
+        newDateFormat = '0' + updateDate.date;
+      }
+
+      const hasEventOnThisDailyPage = (
+        date[0] === String(year) &&
+        date[1] === String(updateDate.monthDaily + 1) &&
+        date[2] === newDateFormat
+      );
+
+      if (hasEventOnThisDailyPage) {
+        let timeArray = [addedEvent[keys].startTime, addedEvent[keys].endTime];
+        todayEventTime.push(timeArray);
+      }
+    }
+
+    for (let i = 0; i < todayEventTime.length; i++) {
+      if (todayEventTime[i][0].includes('30')) {
+        startTime = Number(todayEventTime[i][0].slice(0, 2)) + 0.5;
+      } else startTime = Number(todayEventTime[i][0].slice(0, 2));
+
+      if (todayEventTime[i][1].includes('30')) {
+        endTime = Number(todayEventTime[i][1].slice(0, 2)) + 0.5;
+      } else endTime = Number(todayEventTime[i][1].slice(0, 2));
+
+      let eventTimeIndex = time.indexOf(startTime);
+
+      for (let i = 0; i < time.indexOf(endTime) - time.indexOf(startTime); i++) {
+        eventTimeIndexArray.push(eventTimeIndex);
+        eventTimeIndex++;
+      }
+    }
+
+    const unitTime = time.map((time, i) => {
+      let eventChecking = 'UnitTime';
+      if (eventTimeIndexArray[0] === i) {
+        eventChecking = 'EventUnitTime';
+        eventTimeIndexArray.shift();
+      }
+
       return (
-        <div key={time} className={styles.UnitTime}> </div>
+        <div key={i} className={styles[eventChecking]}> </div>
       );
     });
 
@@ -41,8 +90,7 @@ function Timeline({ showDailyPage, ...props }) {
     });
 
     const weeklySchedule = week.map((week, i) => {
-      const todayCheking = (day === i + 1 || day - 1 === i) ? "TodayWeekScheduleBox" : "WeekScheduleBox";
-      console.log(todayCheking)
+      const todayCheking = (day === i + 1 || day - 1 === i) ? 'TodayWeekScheduleBox' : 'WeekScheduleBox';
       return (
         <div key={week} className={styles[todayCheking]}>
           {unitTime}
