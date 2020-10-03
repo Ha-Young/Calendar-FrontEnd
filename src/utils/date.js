34dbc0cc7305a60b08ffceec86a040ea8a5e7932
Minjieun <chewchewchew3@gmail.com>
2020-10-03 14:42:08 +0900

    delete unused util

--- src/utils/date.js
+++ src/utils/date.js
@@ -7,8 +7,6 @@ import {
 
 export const now = new Date();
 
-export const today = formatFn(now, 'd');
-
 export const getWeeks = (date = now) => eachDayOfInterval({
   start: startOfWeek(date),
   end: endOfWeek(date),
