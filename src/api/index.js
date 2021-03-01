87a04326d8895e6aa5743119550afd62b104ba65
yunsu <than4502@gmail.com>
2021-03-02 01:09:41 +0900

    Add exercise Firebase

--- src/api/index.js
+++ src/api/index.js
@@ -8,5 +8,34 @@ export async function saveSampleData() {
   // Reference: https://firebase.google.com/docs/database/web/read-and-write#receive_a_promise
   await database.ref("test/123").set({
     test: "text",
+    hello: "hi",
+    text: "dummy",
+  });
+}
+
+export async function updateSample() {
+  const database = firebase.database();
+
+  await database.ref("test").child("123").update({test: "Change?"});
+}
+
+export async function addSample() {
+  const database= firebase.database();
+
+  const newKey = await database.ref("test").push().key;
+
+  const updates = {};
+
+  updates["/test/123/" + newKey] = "value";
+
+  await database.ref().update(updates);
+}
+
+export async function addfolder() {
+  const databaseRef = firebase.database().ref();
+
+  await databaseRef.on("value", (snapShot) => {
+    const data = snapShot.val();
+    console.log(data);
   });
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import App from "../components/App/App";
-import { saveSampleData } from "../api";
+import { saveSampleData, addfolder, updateSample, addSample } from "../api";
 
 const mapStateToProps = (state) => ({
   something: "Mapping redux state to App component props.",
@@ -9,7 +9,10 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = () => ({
   // This function is passed to App component.
   onInitialLoad: () => {
-    saveSampleData();
+    //saveSampleData();
+    //updateSample();
+    //addSample();
+    //addfolder();
   },
 });
 
