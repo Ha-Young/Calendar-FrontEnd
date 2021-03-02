a0aea0942f9483e80451f978c21233af6a416b06
onemo <wonmotime1@naver.com>
2021-03-02 14:11:13 +0900

    working weekly calendar

--- src/components/Main/WeeklyCalendar.js
+++ src/components/Main/WeeklyCalendar.js
@@ -2,29 +2,39 @@ import React from 'react';
 
 
 const WeeklyCalendar = () => {
-  const days = ['일', '월', '화', '수', '목', '금', '토', '일'];
+  const days = ['일', '월', '화', '수', '목', '금', '토'];
+  const date = new Date();
+  const thisDay = date.getDay();
+  const thisDate = date.getDate();
   const times = [];
-  const eventCell = [];
+  const events = [];
 
   for (let i = 0; i < 24; i++) {
     times.push(i);
-
-    if (i < 7) {
-      eventCell.push(i);
-    }
+    events.push(i);
   }
-
-  const daysList = days.map((day, index) => {
-    return (
-      <div className='day' key={index}>{day}</div>
-    )
-  });
 
   const timeList = times.map((time, index) => {
     return (
-      <div className='dayEvent'>
+      <div className='timeable'>
         <div className='time' key={index}>{time}</div>
-        <div>event</div>
+      </div>
+    )
+  });
+
+  const eventList = events.map((event, index) => {
+    return (
+      <div className='eventTable'>
+        <div className='event' key={index}>event</div>
+      </div>
+    )
+  });
+
+  const daysList = days.map((day, index) => {
+    return (
+      <div className='week-day' key={index}>
+        <div>{day}</div>
+        <div>{eventList}</div>
       </div>
     )
   });
@@ -32,10 +42,11 @@ const WeeklyCalendar = () => {
   return (
     <div className='calendar'>
       <div className='days'>
+        <div>
+          <div>TimeTable</div>
+          {timeList}
+        </div>
         {daysList}
-      </div>
-      <div className='times'>
-        {timeList}
       </div>
     </div>
   )
