8aa84665b8f1d466b1d5a5bc6c3dcebdca77766c
KimJaedeok <64330699+jdcoder27@users.noreply.github.com>
2021-03-02 12:23:05 +0900

    firebase setting

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -2,15 +2,25 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 
+const SETTING = {
+  API_KEY: process.env.API_KEY,
+  AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+  DATA_BASE_URL: process.env.DATA_BASE_URL,
+  PROJECT_ID: "vanilla-calendar-96dfa",
+  STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+  MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+  APP_ID: process.env.APP_ID,
+}
+
 // TODO: Enter your own config object
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: SETTING.API_KEY,
+  authDomain: SETTING.AUTH_DOMAIN,
+  databaseURL: SETTING.DATABASE_URL,
+  projectId: SETTING.PROJECT_ID,
+  storageBucket: SETTING.STORAGE_BUCKET,
+  messagingSenderId: SETTING.MESSAGING_ID,
+  appId: SETTING.APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
