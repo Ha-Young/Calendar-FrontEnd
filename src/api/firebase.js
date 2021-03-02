d4a8e52dea0053fe1bad1af272e49fd8062004b6
kwon, minho <minhob38@gmail.com>
2021-03-02 16:04:15 +0900

    Add Firebase API Info.

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 
 // TODO: Enter your own config object
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCIgtbQFASJKG4WQ-_HXlyWO-4hS9SVsJw",
+  authDomain: "calendar-d255d.firebaseapp.com",
+  projectId: "calendar-d255d",
+  storageBucket: "calendar-d255d.appspot.com",
+  messagingSenderId: "802260962582",
+  appId: "1:802260962582:web:bc80f12ea779acfd485437",
+  measurementId: "G-028E39V14H"
 };
 
 firebase.initializeApp(firebaseConfig);
