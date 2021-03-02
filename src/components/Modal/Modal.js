ccab32a5bfa4188009bbf908daed9281c27d6854
onemo <wonmotime1@naver.com>
2021-03-03 00:24:49 +0900

    working on modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Modal.module.css';
 
 const Modal = () => {
+  let startOptionList = [];
+  let endOptionList = [];
+
+  for (let i = 0; i < 24; i++) {
+    startOptionList.push(i);
+  }
+
+  for (let i = 1; i < 25; i++) {
+    endOptionList.push(i);
+  }
+
+  startOptionList = startOptionList.map((time, index) => {
+    return (
+      <option
+        value={time}
+        key={index}
+      >
+        {time}
+      </option>
+    )
+  });
+
+  endOptionList = endOptionList.map((time, index) => {
+    return (
+      <option
+        value={time}
+        key={index}
+      >
+        {time}
+      </option>
+    )
+  });
 
   return (
     <div className={styles.modalWrapper}>
@@ -9,23 +41,33 @@ const Modal = () => {
         <input
           type='text'
           value='title'
+          onChange={() => {}}
+          className={styles.titleInput}
         />
       </div>
       <div className={styles.descriptionWrapper}>
         <input
           type='text'
           value='description'
+          onChange={() => {}}
+          className={styles.descriptionInput}
         />
       </div>
-      <div className={styles.dateWrapper}>
+      <div className={styles.dateSelectWrapper}>
         <div>
-          <select name='startTime'>
-
+          <select
+            name='startTime'
+            className={styles.startTimeSelect}
+          >
+            {startOptionList}
           </select>
         </div>
         <div>
-          <select name='endTime'>
-
+          <select
+            name='endTime'
+            className={styles.endTimeSelect}
+          >
+            {endOptionList}
           </select>
         </div>
       </div>
@@ -33,6 +75,7 @@ const Modal = () => {
         <input
           type='submit'
           value='submit'
+          className={styles.submitButton}
         />
       </div>
     </div>
