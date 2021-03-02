e58c64956eae8f59bc9634953b03ba16208a7024
ParkJeongbeen <pjb6510@gmail.com>
2021-03-03 00:25:40 +0900

    add new event

--- src/components/Events/NewEvent.js
+++ src/components/Events/NewEvent.js
@@ -12,11 +12,14 @@ export default function NewEvent() {
   return (
     <div className={styles.NewEvent}>
       <form className={styles.NewEventForm}>
-        <div className={styles.inputTitleBox}></div>
-        <label htmlFor="event-title">Title :</label>
-        <input type="text" name="event-title" />
-        <label htmlFor="event-description">Description</label>
-        <input type="text" name="event-description"/>
+        <div className={styles.InputTitleBox}>
+          <label htmlFor="event-title">Title :</label>
+          <input type="text" name="event-title" />
+        </div>
+        <div className={styles.InputDescriptionBox}>
+          <label htmlFor="event-description">Description</label>
+          <input type="text" name="event-description"/>
+        </div>
       </form>
     </div>
   );
