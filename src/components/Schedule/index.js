ca8fab957adb51271ea5bae73e00337e860bfc07
ha-young <hayeong28@gmail.com>
2021-03-03 23:05:14 +0900

    add: Schedule Contaimer for eventList each day

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -8,20 +8,23 @@ import TimeColumn from "../TimeColumn";
 import WeeklySchedule from "../WeeklySchedule";
 import styles from "./Schedule.module.css";
 
-function Schedule({ viewOption, currentDate }) {
-  // 여기 있어도 되는걸까? 질문하기.
+function Schedule({ viewOption, currentDate, eventListEachDay }) {
   const history = useHistory();
 
   function moveAddEventPage() {
     history.push("/events/new");
-  };
+  }
 
   return (
     <div className={styles.wrapper}>
       <TimeColumn />
-      {viewOption === VIEW_OPTION.DAILY && <DailySchedule date={currentDate}/>}
-      {viewOption === VIEW_OPTION.WEEKLY && <WeeklySchedule currentDate={currentDate}/>}
-      <ScheduleAddBtn onClick={moveAddEventPage}/>
+      {viewOption === VIEW_OPTION.DAILY && (
+        <DailySchedule date={currentDate} eventList={eventListEachDay[currentDate]}/>
+      )}
+      {viewOption === VIEW_OPTION.WEEKLY && (
+        <WeeklySchedule currentDate={currentDate} eventListEachDay={eventListEachDay}/>
+      )}
+      <ScheduleAddBtn onClick={moveAddEventPage} />
     </div>
   );
 }
