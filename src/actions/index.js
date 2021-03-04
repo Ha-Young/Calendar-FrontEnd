a891db0ed502db286396afab990d0a6e41109414
Hansol Yun <hsyoon315@gmail.com>
2021-03-05 04:11:07 +0900

    Fix actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,10 @@
   ref: https://github.com/reduxjs/redux/blob/master/examples/shopping-cart/src/actions/index.js
 
  */
+
+export const addEvent = (eventInfo) => {
+  return {
+    type: "ADD_EVENT",
+    eventInfo
+  }
+}
