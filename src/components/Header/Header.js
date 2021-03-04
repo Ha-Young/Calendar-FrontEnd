4ed6ead305cd11ca955a45bb9f54d1d32125c08a
Hansol Yun <hsyoon315@gmail.com>
2021-03-05 04:08:30 +0900

    Add links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 // TODO: Create your own header.
-export default function Header () {
+function Header () {
   return (
     <header>
       <nav>
-        <ul>
-          <li><Link to='/'>Menu 1</Link></li>
-          <li><Link to='/event'>Menu 2</Link></li>
-        </ul>
+        <div>
+          <button><Link to='/event'>Day</Link></button>
+          <button><Link to='/event/new'>Form</Link></button>
+          <button><Link to='/'>Week</Link></button>
+        </div>
       </nav>
     </header>
   );
 }
+
+
+// <button><Link to='/event/week'>Week</Link></button>
+// function mapStateToProps(state) {
+//   return {
+//     title: state.title,
+//     content: state.content,
+//     date: state.date,
+//     startTime: state.startTime,
+//     endTime: state.endTime,
+//   };
+// }
+
+export default Header;
