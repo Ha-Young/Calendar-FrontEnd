b4d59663d0d20e6d4bd1c06aecc3c3e137aa24b4
Minjieun <chewchewchew3@gmail.com>
2021-03-04 20:33:19 +0900

    need change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./App.module.css";
 import Header from "../Header/Header";
 import SideBar from "../SideBar/SideBar";
--- src/routes/CalendarPage/CalendarPage.js
+++ src/routes/CalendarPage/CalendarPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import Board from "../../components/Board/Board";
 import styles from "./CalendarPage.module.css";
 
@@ -7,7 +6,7 @@ const CalendarPage = ({ onLoad, getEventByCurrentDate, dateList }) => { // togle
   useEffect(() => {
     onLoad();
   }, []);
-  return ( // 감싸줘야 하나..?, value이용해서 해야 하나..? usecallback 이용
+  return (
     <div className={styles.wrapper}>
       <Board getEventByCurrentDate={getEventByCurrentDate} headerData={dateList} />
     </div>
