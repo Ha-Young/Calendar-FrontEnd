97353c14d4cdb0ba71075c340df11dc91a9c17b9
ha-young <hayeong28@naver.com>
2021-03-04 23:11:20 +0900

    add: get data from firebase only one date

--- src/actions/date.js
+++ src/actions/date.js
@@ -1,2 +1,15 @@
 import * as types from "../constants/actionTypes";
 
+export const receiveDate = date => {
+  return {
+    type: types.RECEIVE_DATE,
+    payload: date,
+  };
+};
+
+export const receiveDateList = dateList => {
+  return {
+    type: types.RECEIVE_DATE_LIST,
+    payload: dateList,
+  };
+};
--- src/reducers/date.js
+++ src/reducers/date.js
@@ -1,22 +1,22 @@
 import { combineReducers } from "redux";
 
-import { CHANGE_DATE, CHANGE_VIEW_OPTION, CREATE_EVNETS,  RECEIVE_EVENTS } from "../constants/actionTypes";
+import { CHANGE_DATE, CHANGE_VIEW_OPTION, CREATE_EVNETS,  RECEIVE_DATE,  RECEIVE_EVENTS } from "../constants/actionTypes";
 import { VIEW_OPTION } from "../constants/stateTypes";
 import { getCurrentDateStr, getWeekDateListBasedOnDate } from "../utils/date";
 
 const initialStatus_byId = {
-  "2021-03-03": {
-    id: "2021-03-03",
-    events: ["2021-03-03_12:2"],
-  },
-  "2021-03-01": {
-    id: "2021-03-01",
-    events: ["2021-03-01_12:2"],
-  },
-  "2021-03-04": {
-    id: "2021-03-04",
-    events: ["2021-03-04_13:4", "2021-03-04_18:3"],
-  },
+  // "2021-03-03": {
+  //   id: "2021-03-03",
+  //   events: ["2021-03-03_12:2"],
+  // },
+  // "2021-03-01": {
+  //   id: "2021-03-01",
+  //   events: ["2021-03-01_12:2"],
+  // },
+  // "2021-03-04": {
+  //   id: "2021-03-04",
+  //   events: ["2021-03-04_13:4", "2021-03-04_18:3"],
+  // },
 };
 
 function byId(state = initialStatus_byId, action) {
@@ -37,8 +37,25 @@ function byId(state = initialStatus_byId, action) {
       }
       return newState;
 
-    case RECEIVE_EVENTS:
-      console.log('receive data', state, action);
+    case RECEIVE_DATE:
+      if (action.payload) {
+        const [dateKey] = Object.keys(action.payload);
+        const eventKeys = Object.keys(action.payload[dateKey]);
+
+        const prevEventList = state[dateKey] ? state[dateKey].events : [];
+
+        const newDateById = {
+          id: dateKey,
+          events: [...prevEventList],
+        };
+
+        for (const eventKey of eventKeys) {
+          newDateById.events.push(eventKey);
+        }
+
+        return newDateById;
+      }
+
       return state;
 
     default:
