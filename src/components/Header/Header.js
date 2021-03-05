e3851b6b28b7657cffa5b158529d33d2eaed6ddf
Hansol Yun <hsyoon315@gmail.com>
2021-03-05 19:02:43 +0900

    Remove week link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,23 +9,10 @@ function Header () {
         <div>
           <button><Link to='/event'>Day</Link></button>
           <button><Link to='/event/new'>Form</Link></button>
-          <button><Link to='/'>Week</Link></button>
         </div>
       </nav>
     </header>
   );
 }
-
-
-// <button><Link to='/event/week'>Week</Link></button>
-// function mapStateToProps(state) {
-//   return {
-//     title: state.title,
-//     content: state.content,
-//     date: state.date,
-//     startTime: state.startTime,
-//     endTime: state.endTime,
-//   };
-// }
 
 export default Header;
