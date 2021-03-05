77b97c7b594bcd17f5b792f4ee21ad979fe0f8e9
seongjuseo <canwefly89@gmail.com>
2021-03-05 17:59:16 +0900

    Doing: styling...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import styles from "./Header.module.css";
+import { Link, useHistory } from "react-router-dom";
+import styles from "./Header.module.css";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import routes from "constants/routes";
 
 export default function Header() {
+  const history = useHistory();
   return (
     <header>
       <nav>
-        <button>
-          <Link to="/">Home</Link>
+        <button
+          onClick={() => history.push(routes.DAILY)}
+          className={styles.dailyButton}
+        >
+          일간
         </button>
-        <button>
-          <Link to="/daily">Daily</Link>
+        <button
+          onClick={() => history.push(routes.WEEKLY)}
+          className={styles.weeklyButton}
+        >
+          주간
         </button>
-        <button>
-          <Link to="/weekly">Weekly</Link>
+        <button
+          onClick={() => history.push(routes.ADD_EVENT)}
+          className={styles.addButton}
+        >
+          <FontAwesomeIcon
+            className={(styles.prev_icon, styles.icon)}
+            icon={faPlusSquare}
+          />
         </button>
-        <button>
-          <Link to="/events/new">New Event</Link>
-        </button>
-        <button>
+        <button className={styles.profileButton}>
           <Link to="/profile">Profile</Link>
         </button>
       </nav>
