4f958caff16691f15404e65de59b122ec2018802
seongjuseo <canwefly89@gmail.com>
2021-03-06 03:06:08 +0900

    input validation, input style, header style

--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -12,6 +12,7 @@ const EventForm = ({ type, weeklyEvent, onSubmit }) => {
   const [date, setDate] = useState(getDateISO(0));
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
   const { eventId } = useParams();
 
   useEffect(() => {
@@ -29,26 +30,56 @@ const EventForm = ({ type, weeklyEvent, onSubmit }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const parsedStartTime = parseInt(startTime.slice(0, 2));
-    const parsedEndTime = parseInt(endTime.slice(0, 2));
-    const newEvent = {
-      title,
-      description,
-      date,
-      startTime: parsedStartTime,
-      endTime: parsedEndTime,
-    };
 
-    if (type === typeConst.EDIT) {
-      newEvent.id = eventId;
+    if (validateInput()) {
+      const parsedStartTime = parseInt(startTime.slice(0, 2));
+      const parsedEndTime = parseInt(endTime.slice(0, 2));
+      const newEvent = {
+        title,
+        description,
+        date,
+        startTime: parsedStartTime,
+        endTime: parsedEndTime,
+      };
+
+      if (type === typeConst.EDIT) {
+        newEvent.id = eventId;
+      }
+
+      onSubmit(newEvent, type);
+
+      setTitle("");
+      setDescription("");
+      setDate(getDateISO(0));
+      setStartTime("");
+      setEndTime("");
+    } else {
+      return;
     }
+  };
 
-    onSubmit(newEvent, type);
-
-    setDescription("");
-    setDate(getDateISO(0));
-    setStartTime("");
-    setEndTime("");
+  const validateInput = () => {
+    if (title === "") {
+      setAlertMessage("제목을 입력하세요");
+      return false;
+    } else if (description === "") {
+      setAlertMessage("내용을 입력하세요");
+      return false;
+    } else if (startTime === "") {
+      setAlertMessage("시작 시간을 입력하세요");
+      return false;
+    } else if (endTime === "") {
+      setAlertMessage("종료 시간을 입력하세요");
+      return false;
+    } else if (
+      parseInt(startTime.slice(0, 2)) >= parseInt(endTime.slice(0, 2))
+    ) {
+      setAlertMessage("종료 시간은 시작 시간 이후여야 합니다");
+      return false;
+    } else {
+      setAlertMessage("");
+      return true;
+    }
   };
 
   const handleInput = (event) => {
@@ -83,54 +114,86 @@ const EventForm = ({ type, weeklyEvent, onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        name="title"
-        type="text"
-        placeholder="Event Title"
-        value={title}
-        onChange={(event) => handleInput(event)}
-        required
-      />
-      <input
-        name="description"
-        type="textarea"
-        placeholder="Event Description"
-        value={description}
-        onChange={(event) => handleInput(event)}
-        required
-      />
-      <input
-        name="date"
-        type="date"
-        value={date}
-        onChange={(event) => handleInput(event)}
-        required
-      />
-      <select
-        name="startTime"
-        id="start-time"
-        value={startTime}
-        onChange={(event) => handleInput(event)}
-        required
-      >
-        {dateConst.TIME_LIST.map((time) => (
-          <option key={time}>{time}시</option>
-        ))}
-      </select>
-      <select
-        name="endTime"
-        id="end-time"
-        onChange={(event) => handleInput(event)}
-        value={endTime}
-        required
-      >
-        {dateConst.TIME_LIST.map((time) => (
-          <option key={time}>{time}시</option>
-        ))}
-      </select>
-      <button>Add</button>
-    </form>
+    <>
+      <h1 className={styles.formTitle}>
+        {type === typeConst.ADD ? "이벤트 추가하기" : "이벤트 수정하기"}
+      </h1>
+      <section className={styles.formContainer}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.inputContainer}>
+            <span className={styles.inputType}>제목</span>
+            <input
+              className={styles.title}
+              name="title"
+              type="text"
+              placeholder="Event Title"
+              value={title}
+              onChange={(event) => handleInput(event)}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <span className={styles.inputType}>상세</span>
+            <input
+              className={styles.description}
+              name="description"
+              type="textarea"
+              placeholder="Event Description"
+              value={description}
+              onChange={(event) => handleInput(event)}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <span className={styles.inputType}>날짜</span>
+            <input
+              className={styles.date}
+              name="date"
+              type="date"
+              value={date}
+              onChange={(event) => handleInput(event)}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <span className={styles.inputType}>시작</span>
+            <select
+              className={styles.startTime}
+              name="startTime"
+              id="start-time"
+              value={startTime}
+              onChange={(event) => handleInput(event)}
+              required
+            >
+              {dateConst.TIME_LIST.map((time) => (
+                <option key={time}>{time}시</option>
+              ))}
+            </select>
+          </div>
+          <div className={styles.inputContainer}>
+            <span className={styles.inputType}>종료</span>
+            <select
+              className={styles.endTime}
+              name="endTime"
+              id="end-time"
+              onChange={(event) => handleInput(event)}
+              value={endTime}
+              required
+            >
+              {dateConst.TIME_LIST.map((time) => (
+                <option key={time}>{time}시</option>
+              ))}
+            </select>
+          </div>
+          {alertMessage && (
+            <p className={styles.alertMessage}>{alertMessage}</p>
+          )}
+          <div className={styles.buttonContainer}>
+            <button className={styles.addButton}>제출하기</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,41 @@
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import styles from "./Header.module.css";
-import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import routes from "constants/routes";
 
 export default function Header() {
   const history = useHistory();
   return (
     <header>
-      <nav>
-        <button
-          onClick={() => history.push(routes.DAILY)}
-          className={styles.dailyButton}
-        >
-          일간
-        </button>
-        <button
-          onClick={() => history.push(routes.WEEKLY)}
-          className={styles.weeklyButton}
-        >
-          주간
-        </button>
-        <button
-          onClick={() => history.push(routes.ADD_EVENT)}
-          className={styles.addButton}
-        >
-          <FontAwesomeIcon
-            className={(styles.prev_icon, styles.icon)}
-            icon={faPlusSquare}
-          />
-        </button>
-        <button className={styles.profileButton}>
-          <Link to="/profile">Profile</Link>
-        </button>
+      <nav className={styles.navContainer}>
+        <div className={styles.calenderTypeButtons}>
+          <button
+            onClick={() => history.push(routes.DAILY)}
+            className={styles.dailyButton}
+          >
+            일간
+          </button>
+          <button
+            onClick={() => history.push(routes.WEEKLY)}
+            className={styles.weeklyButton}
+          >
+            주간
+          </button>
+        </div>
+        <div className={styles.optionButtons}>
+          <button
+            onClick={() => history.push(routes.ADD_EVENT)}
+            className={styles.addButton}
+          >
+            이벤트 생성
+          </button>
+          <button
+            onClick={() => history.push(routes.PROFILE)}
+            className={styles.profileButton}
+          >
+            프로필
+          </button>
+        </div>
       </nav>
     </header>
   );
