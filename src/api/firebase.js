6d602eb44985b2cf3d0be990f958ce384de56173
eluka22 <eluka22@gmail.com>
2021-03-05 16:49:40 +0900

    Add environment variables

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,16 +1,18 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+import dotenv from "dotenv";
+dotenv.config();
 
 // TODO: Enter your own config object
 const firebaseConfig = {
-  apiKey: "AIzaSyAH8Uul5ZFOvVc0Tdh4ZA0SFPkDNcFSs88",
-  authDomain: "calendar-viewer-b602e.firebaseapp.com",
-  databaseURL: "https://calendar-viewer-b602e-default-rtdb.firebaseio.com/",
-  projectId: "calendar-viewer-b602e",
-  storageBucket: "calendar-viewer-b602e.appspot.com",
-  messagingSenderId: "95835663215",
-  appId: "1:95835663215:web:b60f6ca81e17d90da1569a",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
