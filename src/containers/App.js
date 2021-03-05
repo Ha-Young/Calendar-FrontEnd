ac316513db6a3be343367eff8430dde398d1d0de
Hansol Yun <hsyoon315@gmail.com>
2021-03-06 04:01:08 +0900

    Add semicolon

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   onEventInfo: (eventInfo) => {
-    dispatch(addEvent(eventInfo))
+    dispatch(addEvent(eventInfo));
   },
 });
 
