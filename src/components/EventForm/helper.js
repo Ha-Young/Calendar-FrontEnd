afdc4f7db289cb4734813cad6ece393d51a69a22
ha-young <hayeong28@naver.com>
2021-03-05 14:19:34 +0900

    add: event update, delete reducer process

--- src/components/EventForm/helper.js
+++ src/components/EventForm/helper.js
@@ -3,6 +3,10 @@ import { message } from "antd";
 import { DATE_FORMAT, DATE_FORMAT_WITH_HOUR } from "../../constants/common";
 import { checkIsSameDate, checkIsSameDay, getDiffHour } from "../../utils/date";
 
+export const FORM_DEFAULT_TITLE = "일정";
+export const FORM_TYPE_CREATE = "추가";
+export const FORM_TYPE_UPDATE = "수정";
+export const FORM_TYPE_DELETE = "삭제";
 export const ERROR_TYPE_OVER_START_DAY = "OverStartDayError";
 export const ERROR_TYPE_EVENT_DAY_OVER = "EventDayOverError";
 
--- src/components/EventForm/index.js
+++ src/components/EventForm/index.js
@@ -1,18 +1,27 @@
 import { Button, DatePicker, Form, Input } from "antd";
 import React from "react";
-import { useHistory } from "react-router-dom";
 
-import { ERROR_MSG_EVENT_DAY_OVER, ERROR_MSG_OVER_START_DAY } from "../../constants/errorMsg";
-import { getCurrentMoment } from "../../utils/date";
+import {
+  ERROR_MSG_EVENT_DAY_OVER,
+  ERROR_MSG_OVER_START_DAY,
+} from "../../constants/errorMsg";
+import { getMoment } from "../../utils/date";
 import styles from "./EventForm.module.css";
-import { ERROR_TYPE_EVENT_DAY_OVER, ERROR_TYPE_OVER_START_DAY, layout, makeNewEvent, viewErrMsg } from "./helper";
+import {
+  ERROR_TYPE_EVENT_DAY_OVER,
+  ERROR_TYPE_OVER_START_DAY,
+  FORM_DEFAULT_TITLE,
+  FORM_TYPE_CREATE,
+  FORM_TYPE_DELETE,
+  FORM_TYPE_UPDATE,
+  layout,
+  makeNewEvent,
+  viewErrMsg,
+} from "./helper";
 
-function EventForm({ userId, onCreate, event }) {
-  const history = useHistory();
-
+function EventForm({ onSubmit, onCancel, onDelete, event }) {
   function handleSubmitClick(values) {
-    const event = values;
-    const { newEvent, errorType } = makeNewEvent(event);
+    const { newEvent, errorType } = makeNewEvent(values);
 
     if (!newEvent) {
       //todo. constant화
@@ -25,55 +34,66 @@ function EventForm({ userId, onCreate, event }) {
     }
 
     // 질문 userId도 같이?
-    onCreate({
-      userId,
-      event: newEvent,
-    });
-
-    history.push('/calendar');
-  };
+    onSubmit(newEvent);
+  }
 
   function handleCancelClick() {
-    history.goBack();
-  };
+    onCancel();
+  }
+
+  function handleDeleteClick() {
+    onDelete({
+      eventId: event.id,
+      date: event.date,
+    });
+  }
+
+  const initialValues = event
+    ? {
+      title: event.title,
+      description: event.description,
+      startDate: getMoment(event.startDate),
+      endDate: getMoment().add(1, "hour"),
+    }
+    : {
+      title: "",
+      description: "",
+      startDate: getMoment(),
+      endDate: getMoment().add(1, "hour"),
+    };
 
   return (
     <>
-      <h2>이벤트 추가</h2>
+      <h2>{`${FORM_DEFAULT_TITLE} ${event ? FORM_TYPE_UPDATE : FORM_TYPE_CREATE}`}</h2>
       <Form
         {...layout}
         className={styles.addForm}
         onFinish={handleSubmitClick}
-        initialValues={{
-          startDate: getCurrentMoment(),
-          endDate: getCurrentMoment().add(1, 'hour'),
-        }}
+        initialValues={initialValues}
       >
+        {event && <Button onClick={handleDeleteClick} danger>{FORM_TYPE_DELETE}</Button>}
         <Form.Item
           label="제목"
-          name='title'
+          name="title"
           rules={[
             {
               required: true,
-              message: '제목을 입력해주세요',
+              message: "제목을 입력해주세요",
             }
           ]}
         >
           <Input />
         </Form.Item>
-        <Form.Item
-          label="설명"
-          name='description'
-        >
+        <Form.Item label="설명" name="description">
           <Input.TextArea />
         </Form.Item>
         <Form.Item
           label="시작날짜"
-          name='startDate'
+          name="startDate"
           rules={[
             {
               required: true,
-              message: '시작날짜를 입력해주세요',
+              message: "시작날짜를 입력해주세요",
             }
           ]}
         >
@@ -85,11 +105,11 @@ function EventForm({ userId, onCreate, event }) {
         </Form.Item>
         <Form.Item
           label="종료날짜"
-          name='endDate'
+          name="endDate"
           rules={[
             {
               required: true,
-              message: '종료날짜를 입력해주세요',
+              message: "종료날짜를 입력해주세요",
             }
           ]}
         >
@@ -104,12 +124,12 @@ function EventForm({ userId, onCreate, event }) {
           className={styles.btnWrapper}
         >
           <Button type="primary" htmlType="submit">
-            생성
+            {event ? FORM_TYPE_UPDATE : FORM_TYPE_CREATE}
           </Button>
           <Button
             htmlType="button"
             style={{
-              margin: '0 10px',
+              margin: "0 10px",
             }}
             onClick={handleCancelClick}
           >
