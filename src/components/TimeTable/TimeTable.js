40a72584cb7075c385371799914217eeac1e4e3c
Hansol Yun <hsyoon315@gmail.com>
2021-03-05 17:03:01 +0900

    Add timetable component

---
+++ src/components/TimeTable/TimeTable.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Time.module.css";
+
+function Time() {
+  const times = ["1시", "2시", "3시", "4시", "5시", "6시", "7시", "8시", "9시", "10시", 
+                 "11시", "12시", "13시", "14시", "15시", "16시", "17시", "18시", "19시",
+                 "20시", "21시", "22시", "23시"];
+
+  return (
+    <div className={styles.div} >
+      {times.map((time) => {
+        return (<div className={styles.time} key={time}>{time}</div>);
+      })}
+    </div>
+  );
+}
+
+export default Time;
