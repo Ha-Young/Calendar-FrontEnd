37263b0d2fbe3bfdacd6f804990a2e05865002cb
hee-c <ways2kim@gmail.com>
2021-03-06 00:46:25 +0900

    Fix css styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
-import { IoMenuOutline } from "react-icons/io"
+import { IoMenuOutline, IoSettingsOutline } from "react-icons/io5";
+import { BsQuestionCircle } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { CgMenuGridO } from "react-icons/cg";
 import styles from "./Header.module.css";
+import logo from "../../assets/logo.png";
 
 export default function Header({ prevButtonClicked, nextButtonClicked }) {
   return (
     <header className={styles.wrapper}>
       <div className={styles.navItemWrapper}>
-        <Link to="/calendar">Calendar</Link>
+        <button className={styles.changeBtn}>
+          <IoMenuOutline />
+        </button>
       </div>
       <div className={styles.navItemWrapper}>
-        <Link to="/events/new">Create Event</Link>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </div>
+      <div className={styles.navItemWrapper}>
+        <span>캘린더</span>
+      </div>
+      <div className={styles.navItemWrapper}>
+        <button>오늘</button>
       </div>
       <div className={styles.navButtonWrapper}>
         <button className={styles.changeBtn}>
@@ -22,6 +34,12 @@ export default function Header({ prevButtonClicked, nextButtonClicked }) {
         <button className={styles.changeBtn}>
           <MdNavigateNext onClick={() => nextButtonClicked()} />
         </button>
+      </div>
+      <div className={styles.navItemWrapper}>
+        <Link to="/calendar">Calendar</Link>
+      </div>
+      <div className={styles.navItemWrapper}>
+        <Link to="/events/new">Create Event</Link>
       </div>
     </header>
   );
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import Calendar from "react-calendar"
+import Calendar from "react-calendar";
+import MonthView from "react-calendar";
 import styles from "./SideBar.module.css";
 
 export default function SideBar({ selectDay }) {
   return (
     <div className={styles.sidebarWrapper}>
-      <Calendar
-        onClickDay={(date) => selectDay(date)}
-      />
+      <div className={styles.btnWrapper}>
+        <button className={styles.createBtn}>
+          <div>
+            <svg width="36" height="36" viewBox="0 0 36 36">
+              <path fill="#34A853" d="M16 16v14h4V20z"></path>
+              <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+              <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+              <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
+              <path fill="none" d="M0 0h36v36H0z"></path>
+            </svg>
+          </div>
+          <div className={styles.textWrapper}>
+            만들기
+          </div>
+        </button>
+      </div>
+      <div className={styles.calendarWrapper}>
+        <Calendar
+          className={styles.calendar}
+          onClickDay={(date) => selectDay(date)}
+          tileClassName={styles.calendarItem}
+        />
+      </div>
     </div>
   );
 }
