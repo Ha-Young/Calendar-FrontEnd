6d6549069fffabc3ff495abbdd8804e0ae72c825
Hansol Yun <hsyoon315@gmail.com>
2021-03-06 03:49:54 +0900

    Remove connect and fix handlechange code

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { connect } from "react-redux";
-import { addEvent } from "../../actions/index";
 import styles from "./EventForm.module.css";
 
-function EventForm({ addEvent, ...eventInfo }) {
+function EventForm({ onEventInfo }) {
+  const eventInfo = {};
+
   function handleChange(e) {
     switch (e.target.name) {
       case "title":
-        eventInfo.title = e.target.value
+        eventInfo.title = e.target.value;
         break;
       case "content":
         eventInfo.content = e.target.value;
@@ -15,42 +15,32 @@ function EventForm({ addEvent, ...eventInfo }) {
       case "date":
         eventInfo.date = e.target.value;
         break;
-      case "starttime":
+      case "startTime":
         eventInfo.startTime = Number(e.target.value);
         break;
-      case "endtime":
+      case "endTime":
         eventInfo.endTime = Number(e.target.value);
         break;
-      default:
-        return;
+      default :
+        return eventInfo;
     }
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    addEvent(eventInfo);
+    onEventInfo(eventInfo);
   }
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <label>제목<input className={styles.input} type="text" name="title" onChange={handleChange}/></label>
-      <label>내용<input className={styles.input} type="text" name="content" onChange={handleChange}/></label>
-      <label>날짜<input className={styles.input} type="date" name="date" onChange={handleChange}/></label>
-      <label>시작<input className={styles.input} type="number" name="starttime" onChange={handleChange}/></label>
-      <label>종료<input className={styles.input} type="number" name="endtime" onChange={handleChange}/></label>
+      <label>제목<input className={styles.input} type="text" name="title" onChange={handleChange} /></label>
+      <label>내용<input className={styles.input} type="text" name="content" onChange={handleChange} /></label>
+      <label>날짜<input className={styles.input} type="date" name="date" onChange={handleChange} /></label>
+      <label>시작<input className={styles.input} type="number" name="startTime" onChange={handleChange} /></label>
+      <label>종료<input className={styles.input} type="number" name="endTime" onChange={handleChange} /></label>
       <button type="submmit" className={styles.button}>저장</button>
     </form>
   );
 }
 
-function mapStateToProps(state) {
-  return {
-    title: state.title,
-    content: state.content,
-    date: state.date,
-    startTime: state.startTime,
-    endTime: state.endTime,
-  };
-}
-
-export default connect(mapStateToProps, { addEvent })(EventForm);
+export default EventForm;
