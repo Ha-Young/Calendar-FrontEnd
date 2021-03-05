f72a9f9b70c66f0ca761691b6360e39f559fe164
onemo <wonmotime1@naver.com>
2021-03-05 18:09:41 +0900

    add env

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCg7LdWWFq-pWBsykO-2w5I-bcZP5dvAlo",
-  authDomain: "calendar-viewer-1d702.firebaseapp.com",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: "https://calendar-viewer-1d702-default-rtdb.firebaseio.com",
-  projectId: "calendar-viewer-1d702",
-  storageBucket: "calendar-viewer-1d702.appspot.com",
-  messagingSenderId: "992537971735",
-  appId: "1:992537971735:web:900d54177dc245835e2052"
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
