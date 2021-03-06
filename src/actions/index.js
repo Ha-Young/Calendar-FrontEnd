e047bddf789da81b780a7b1742990f75143918c7
Hansol Yun <hsyoon315@gmail.com>
2021-03-06 17:51:25 +0900

    Use action type constant

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,11 @@
   ref: https://github.com/reduxjs/redux/blob/master/examples/shopping-cart/src/actions/index.js
 
  */
+import { ADD_EVENT } from "../constants/actionTypes";
 
 export const addEvent = (eventInfo) => {
   return {
-    type: "ADD_EVENT",
+    type: ADD_EVENT,
     eventInfo,
   }
 }
