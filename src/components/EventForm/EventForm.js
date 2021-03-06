bde33da6546e9dcbd3b70b5b5fb0ec349d8793c9
Hansol Yun <hsyoon315@gmail.com>
2021-03-07 03:50:19 +0900

    Add event detail so that keep in data in form

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -2,16 +2,17 @@ import React from "react";
 import styles from "./EventForm.module.css";
 import FormInput from "../FormInput/FormInput";
 
-function EventForm({ onEventInfo }) {
+function EventForm({ onEventInfo, eventDetail }) {
   const eventInfo = {};
+  const {title, description, date, startTime, endTime} = eventDetail;
 
   function handleChange(e) {
     switch (e.target.name) {
       case "title":
         eventInfo.title = e.target.value;
         break;
-      case "content":
-        eventInfo.content = e.target.value;
+      case "description":
+        eventInfo.description = e.target.value;
         break;
       case "date":
         eventInfo.date = e.target.value;
@@ -34,11 +35,11 @@ function EventForm({ onEventInfo }) {
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <label>제목<FormInput type={"text"} name={"title"} onChange={handleChange} /></label>
-      <label>내용<FormInput type={"text"} name={"content"} onChange={handleChange} /></label>
-      <label>날짜<FormInput type={"date"} name={"date"} onChange={handleChange} /></label>
-      <label>시작<FormInput type={"number"} name={"startTime"} onChange={handleChange} /></label>
-      <label>종료<FormInput type={"number"} name={"endTime"} onChange={handleChange} /></label>
+      <label>제목<FormInput type={"text"} name={"title"} onChange={handleChange} value={title} /></label>
+      <label>내용<FormInput type={"text"} name={"description"} onChange={handleChange} value={description} /></label>
+      <label>날짜<FormInput type={"date"} name={"date"} onChange={handleChange} value={date} /></label>
+      <label>시작<FormInput type={"number"} name={"startTime"} onChange={handleChange} value={startTime} /></label>
+      <label>종료<FormInput type={"number"} name={"endTime"} onChange={handleChange}  value={endTime} /></label>
       <button type="submmit" className={styles.button}>저장</button>
     </form>
   );
