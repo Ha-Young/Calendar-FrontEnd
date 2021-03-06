4aaf2d9eea950f920abc348b003c73bcb723dddb
Hansol Yun <hsyoon315@gmail.com>
2021-03-06 17:43:25 +0900

    Fix props

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,8 +1,7 @@
 import React from "react";
 import styles from "./FormInput.module.css";
 
-function FormInput(props) {
-  const {type, name, onChange} = props;
+function FormInput({ type, name, onChange }) {
   return (
     <input 
       className={styles.input}
