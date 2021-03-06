1913cb93fbd83d0946ccb5bd4f89023ab9486d2b
Hansol Yun <hsyoon315@gmail.com>
2021-03-07 03:52:07 +0900

    Add Forminput component

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./FormInput.module.css";
 
-function FormInput({ type, name, onChange }) {
+function FormInput({ type, name, onChange, value }) {
   return (
     <input 
       className={styles.input}
       type={type}
       name={name}
       onChange={onChange}
+      value={value}
     />
   );
 }
