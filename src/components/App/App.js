20bf949e6241a14ade3c297cd9ba6d6344c45d66
Hansol Yun <hsyoon315@gmail.com>
2021-03-07 03:47:04 +0900

    Add local state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 // TODO: We are using CSS Modules here.
 // Do your own research about CSS Modules.
@@ -7,20 +7,49 @@ import styles from "./App.module.css";
 import Header from "../Header/Header";
 import EventForm from "../EventForm/EventForm";
 import DailyCalendar from "../DailyCalendar/DailyCalendar";
+import dayjs from "dayjs";
 
 function App({ state, onEventInfo }) {
+  const [todayDate, setTodayDate] = useState(dayjs());
+  const [date, setDate] = useState(todayDate.format("YYYY-MM-DD"));
+  const [id, setId] = useState(null);
+
+  function goNextDay() {
+    setTodayDate(todayDate.add(1, "day"));
+  }
+
+  function goPrevDay() {
+    setTodayDate(todayDate.subtract(1, "day"));
+  }
+
+  useEffect(() => {
+    setDate(todayDate.format("YYYY-MM-DD"));
+  }, [todayDate]);
+
   return (
     <div className={styles.App}>
       <Header />
       <Switch>
         <Route path="/calendar" exact>
-          <DailyCalendar eventInfo={state}/>
+          <DailyCalendar
+            eventInfo={state}
+            date={date}
+            goNextDay={goNextDay}
+            goPrevDay={goPrevDay}
+            setId={setId}/>
         </Route>
         <Route path="/event/new">
-          <EventForm onEventInfo={onEventInfo} />
+          <EventForm
+            eventDetail={state.hasOwnProperty(date) && state[date]}
+            onEventInfo={onEventInfo} 
+          />
         </Route>
         <Route path="/event/:eventId">
-          <EventForm />
+          <EventForm
+            eventDetail={state.hasOwnProperty(date) && id !== null && state[date][id]} 
+            currentDate={date}
+            onEventInfo={onEventInfo}
+          />
         </Route>
       </Switch>
     </div>
